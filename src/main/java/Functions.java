94ca3514969e9842d43ffffe37463947fa1b4bd9
Pavel <pgavriluk@gmail.com>
2019-09-01 17:35:32 -0400

    refactored

--- src/main/java/Functions.java
+++ src/main/java/Functions.java
@@ -1,3 +1,5 @@
+package main.java;
+
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+package main.java;
+
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
